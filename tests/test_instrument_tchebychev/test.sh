29b31d6f0d63d2d727a3f0056aebeaa699cb4f70
Pablo de Oliveira <pablo@sifflez.org>
2015-04-21 21:11:07 +0200

    By default comment plot.R command in tests
    Avoids including R as a dependency to run the test suite

--- tests/test_instrument_tchebychev/test.sh
+++ tests/test_instrument_tchebychev/test.sh
@@ -1,3 +1,4 @@
+B
 #!/bin/bash
 set -e
 
@@ -5,13 +6,18 @@ set -e
 
 ../../verificarlo tchebychev.c -o tchebychev
 
-# Run 30 iterations of tchebychev for all values in [.0:1.0:.01]
+# Run 15 iterations of tchebychev for all values in [.0:1.0:.01]
 echo "z y" > output
 for z in $(seq 0.0 0.01 1.0); do
-    for i in $(seq 1 30); do
+    for i in $(seq 1 15); do
         ./tchebychev $z >> output
     done
 done
 
 # Plot the result, resulting graph is saved as Rplots.pdf
-./plot.R
+# By default this line is commented to avoid including R as a
+# dependency for running the test suite.
+# If you want to see the output of the run, install R and run
+# the following command manually.
+
+#./plot.R
