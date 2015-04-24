ff292353a3103c86e7446db816aa755c9f198d23
Pablo de Oliveira <pablo@sifflez.org>
2015-04-25 01:09:25 +0200

    runOnModule should return true when IR is modified

--- src/libmcainstrument/libMCAInstrument.cpp
+++ src/libmcainstrument/libMCAInstrument.cpp
@@ -1,8 +1,6 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Module.h"
-#include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
-#include "llvm/IR/InstrTypes.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/IRBuilder.h"
@@ -13,35 +11,38 @@ namespace {
     struct McalibInst : public ModulePass {
         static char ID;
 
-        McalibInst() : ModulePass(ID) {}
+        McalibInst() : ModulePass(ID) { }
 
-
-        bool runOnModule(Module &M)
-        {
-            for(Module::iterator F = M.begin(), E = M.end(); F!= E; ++F)
-            {
-                runOnFunction(M, *F);
+        bool runOnModule(Module &M) {
+            bool modified = false;
+            for (Module::iterator F = M.begin(), E = M.end(); F != E; ++F) {
+                modified |= runOnFunction(M, *F);
             }
+            // runOnModule must return true if the pass modifies the IR
+            return modified;
         }
+
 
         bool runOnFunction(Module &M, Function &F) {
             errs() << "In Function: ";
             errs().write_escaped(F.getName()) << '\n';
 
+            bool modified = false;
+
             for (Function::iterator bi = F.begin(), be = F.end(); bi != be; ++bi) {
-                runOnBasicBlock(M, *bi);
+                modified |= runOnBasicBlock(M, *bi);
             }
+            return modified;
         }
 
-        Instruction * replaceWithMCACall(Module &M, BasicBlock &B, Instruction &I, std::string opName) {
-            Type * opType = I.getType();
+        Instruction *replaceWithMCACall(Module &M, BasicBlock &B, Instruction &I, std::string opName) {
+            Type *opType = I.getType();
             std::string opTypeName;
 
-
             std::string vectorName = "";
-            Type * baseType = opType;
+            Type *baseType = opType;
             if (opType->isVectorTy()) {
-                VectorType * t = static_cast<VectorType*>(opType);
+                VectorType *t = static_cast<VectorType *>(opType);
                 baseType = t->getElementType();
                 unsigned size = t->getNumElements();
 
@@ -70,77 +71,80 @@ namespace {
                                                        opType,
                                                        opType,
                                                        opType,
-                                                       (Type*)0);
+                                                       (Type *) 0);
 
             IRBuilder<> builder(getGlobalContext());
-            Instruction * newInst = builder.CreateCall2(
-                cast<Function>(hookFunc),
-                I.getOperand(0),
-                I.getOperand(1),
-                "");
+            Instruction *newInst = builder.CreateCall2(
+                    cast<Function>(hookFunc),
+                    I.getOperand(0),
+                    I.getOperand(1),
+                    "");
             return newInst;
         }
 
         std::string cmpOpCode(Instruction &I) {
             FCmpInst::Predicate p = (cast<FCmpInst>(I)).getPredicate();
-            switch(p) {
+            switch (p) {
 
-            case CmpInst::FCMP_OEQ:
-            case CmpInst::FCMP_UEQ:
-                return "eq";
+                case CmpInst::FCMP_OEQ:
+                case CmpInst::FCMP_UEQ:
+                    return "eq";
 
-            case CmpInst::FCMP_OGT:
-            case CmpInst::FCMP_UGT:
-                return "gt";
+                case CmpInst::FCMP_OGT:
+                case CmpInst::FCMP_UGT:
+                    return "gt";
 
-            case CmpInst::FCMP_OGE:
-            case CmpInst::FCMP_UGE:
-                return "ge";
+                case CmpInst::FCMP_OGE:
+                case CmpInst::FCMP_UGE:
+                    return "ge";
 
-            case CmpInst::FCMP_OLT:
-            case CmpInst::FCMP_ULT:
-                return "lt";
+                case CmpInst::FCMP_OLT:
+                case CmpInst::FCMP_ULT:
+                    return "lt";
 
-            case CmpInst::FCMP_OLE:
-            case CmpInst::FCMP_ULE:
-                return "le";
+                case CmpInst::FCMP_OLE:
+                case CmpInst::FCMP_ULE:
+                    return "le";
 
-            case CmpInst::FCMP_ONE:
-            case CmpInst::FCMP_UNE:
-                return "ne";
+                case CmpInst::FCMP_ONE:
+                case CmpInst::FCMP_UNE:
+                    return "ne";
 
-            default:
-                return "";
+                default:
+                    return "";
             }
         }
 
         std::string mustReplace(Instruction &I) {
-            switch(I.getOpcode()) {
-            case Instruction::FAdd:
-                return "add";
-            case Instruction::FSub:
-                // In LLVM IR the FSub instruction is used to represent FNeg
-                return "sub";
-            case Instruction::FMul:
-                return "mul";
-            case Instruction::FDiv:
-                return "div";
-            case Instruction::FCmp:
-                return cmpOpCode(I);
-            default:
-                return "";
+            switch (I.getOpcode()) {
+                case Instruction::FAdd:
+                    return "add";
+                case Instruction::FSub:
+                    // In LLVM IR the FSub instruction is used to represent FNeg
+                    return "sub";
+                case Instruction::FMul:
+                    return "mul";
+                case Instruction::FDiv:
+                    return "div";
+                case Instruction::FCmp:
+                    return cmpOpCode(I);
+                default:
+                    return "";
             }
         }
 
         bool runOnBasicBlock(Module &M, BasicBlock &B) {
+            bool modified = false;
             for (BasicBlock::iterator ii = B.begin(), ie = B.end(); ii != ie; ++ii) {
                 Instruction &I = *ii;
                 std::string opName = mustReplace(I);
                 if (opName == "") continue;
                 errs() << "Instrumenting" << I << '\n';
-                Instruction * newInst = replaceWithMCACall(M, B, I, opName);
+                Instruction *newInst = replaceWithMCACall(M, B, I, opName);
                 ReplaceInstWithInst(B.getInstList(), ii, newInst);
+                modified = true;
             }
+            return modified;
         }
     };
 }
