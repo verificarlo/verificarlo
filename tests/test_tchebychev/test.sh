d5cd4493b5207fec72abab691462de918e313ab2
Pablo de Oliveira Castro <pablo@sifflez.org>
2017-04-24 12:03:42 +0200

    Make test faster

--- tests/test_tchebychev/test.sh
+++ tests/test_tchebychev/test.sh
@@ -12,7 +12,7 @@ verificarlo tchebychev.c -o tchebychev
 # Run 15 iterations of tchebychev for all values in [.0:1.0:.01]
 echo "z y" > output
 for z in $(seq 0.0 0.01 1.0); do
-    for i in $(seq 1 100); do
+    for i in $(seq 1 15); do
         ./tchebychev $z >> output
     done
 done
