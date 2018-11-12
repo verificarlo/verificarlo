43b9b7f8788c6f77f7b631816c8f43471f4cefb5
Pablo Oliveira <pablo@sifflez.org>
2018-11-12 11:46:19 +0100

    Update rr_mode test to check both pow2 and non pow2 exact operations

--- tests/test_rr_mode_exact/test.sh
+++ tests/test_rr_mode_exact/test.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 set -e
-
 export VERIFICARLO_PRECISION=24
 export VERIFICARLO_MCAMODE=RR
 
-# FLOAT
-
-verificarlo -O0 rr_mode.c -o rr_mode
-
-for BACKEND in MPFR QUAD; do
-    export VERIFICARLO_BACKEND=$BACKEND
-    rm -f output_$BACKEND
+for EXP in FLOAT FLOAT_POW2; do
+  verificarlo -D${EXP} -O0 rr_mode.c -o rr_mode
+  for BACKEND in MPFR QUAD; do
+    export VERIFICARLO_BACKEND=${BACKEND}
+    rm -f output_${BACKEND}_${EXP}
     for i in `seq 100`; do
-	./rr_mode >> output_$BACKEND
+      ./rr_mode >> output_${BACKEND}_${EXP}
     done
-    diff output_$BACKEND ref_FLOAT
+    echo Testing $EXP with $BACKEND
+    diff output_${BACKEND}_${EXP} ref_${EXP}
+  done
 done
-    
+
 export VERIFICARLO_PRECISION=53
 export VERIFICARLO_MCAMODE=RR
 
-# DOUBLE
-
-verificarlo -DDOUBLE -O0 rr_mode.c -o rr_mode
-
-for BACKEND in MPFR QUAD; do
-    export VERIFICARLO_BACKEND=$BACKEND
-    rm -f output_$BACKEND
-    for i in `seq 100`; do
-	./rr_mode >> output_$BACKEND
+for EXP in DOUBLE DOUBLE_POW2; do
+  verificarlo -D${EXP} -O0 rr_mode.c -o rr_mode
+  for BACKEND in MPFR QUAD; do
+    export VERIFICARLO_BACKEND=${BACKEND}
+    rm -f output_${BACKEND}_${EXP}
+    for i in `seq 1000`; do
+      ./rr_mode >> output_${BACKEND}_${EXP}
     done
-    diff output_$BACKEND ref_DOUBLE
+    echo Testing $EXP with $BACKEND
+    diff output_${BACKEND}_${EXP} ref_${EXP}
+  done
 done
-    
