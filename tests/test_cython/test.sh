f562c33d6f1036f798ffacf28b5b32ad7a9eae1b
Pablo Oliveira <pablo@sifflez.org>
2019-08-06 15:27:58 +0200

    Fix typo in test_cython/test.sh

--- tests/test_cython/test.sh
+++ tests/test_cython/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if !python3 -c "import cython" 2>/dev/null; then
+if ! python3 -c "import cython" 2>/dev/null; then
     echo "this test is not running without Cython installed"
     exit 0
 fi
