e11365d3ac9ef0afc2cf6ee1feffd9e16fd4b527
Pablo Oliveira <pablo@sifflez.org>
2019-08-08 16:52:31 +0200

    Comment interflop.h

--- src/common/interflop.h
+++ src/common/interflop.h
@@ -1,6 +1,6 @@
 /* interflop backend interface */
 
-/* float compare predicates, follows the same order than
+/* interflop float compare predicates, follows the same order than
  * LLVM's FCMPInstruction predicates */
 enum FCMP_PREDICATE {
   FCMP_FALSE,
@@ -22,27 +22,34 @@ enum FCMP_PREDICATE {
 };
 
 struct interflop_backend_interface_t {
-  void (*interflop_add_float)(float, float, float *, void *);
-  void (*interflop_sub_float)(float, float, float *, void *);
-  void (*interflop_mul_float)(float, float, float *, void *);
-  void (*interflop_div_float)(float, float, float *, void *);
-  void (*interflop_cmp_float)(enum FCMP_PREDICATE, float, float, bool *,
-                              void *);
+  void (*interflop_add_float)(float a, float b, float *c, void *context);
+  void (*interflop_sub_float)(float a, float b, float *c, void *context);
+  void (*interflop_mul_float)(float a, float b, float *c, void *context);
+  void (*interflop_div_float)(float a, float b, float *c, void *context);
+  void (*interflop_cmp_float)(enum FCMP_PREDICATE p, float a, float b, bool *c,
+                              void *context);
 
-  void (*interflop_add_double)(double, double, double *, void *);
-  void (*interflop_sub_double)(double, double, double *, void *);
-  void (*interflop_mul_double)(double, double, double *, void *);
-  void (*interflop_div_double)(double, double, double *, void *);
-  void (*interflop_cmp_double)(enum FCMP_PREDICATE, double, double, bool *,
-                               void *);
+  void (*interflop_add_double)(double a, double b, double *c, void *context);
+  void (*interflop_sub_double)(double a, double b, double *c, void *context);
+  void (*interflop_mul_double)(double a, double b, double *c, void *context);
+  void (*interflop_div_double)(double a, double b, double *c, void *context);
+  void (*interflop_cmp_double)(enum FCMP_PREDICATE p, double a, double b, bool *c,
+                               void *context);
 };
 
 /* interflop_init: called at initialization before using a backend.
  * It returns an interflop_backend_interface_t structure with callbacks
  * for each of the numerical instrument hooks.
  *
- * argv will be deallocated after the call. If the backends, wants to make it
- * persistent it should copy it.
+ * argc: number of arguments passed to the backend
+ *
+ * argv: arguments passed to the backend, argv[0] always contains the name of
+ * the backend library. argv[] may be deallocated after the call to
+ * interflop_init. To make it persistent, a backend must copy it.
+ *
+ * context: the backend is free to make this point to a backend-specific
+ * context. The frontend will pass the context back as the last argument of the
+ * above instrumentation hooks.
  * */
 
 struct interflop_backend_interface_t interflop_init(int argc, char **argv,
