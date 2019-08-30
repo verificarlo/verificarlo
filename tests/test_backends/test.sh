db64be50b9c66e6818a99633299f4ecf0812e20e
yohanchatelain <yohan.chatelain@gmail.com>
2019-08-30 16:08:07 +0200

    Update test.sh of test_backends
           - Fix randomly the seed for testing differents sampling to have a more robust test
           - Tests PB and RR modes
           - Test the precisions 24 and 53 for single and double precision

--- tests/test_backends/test.sh
+++ tests/test_backends/test.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 #set -e
 
+SEED=$RANDOM
+echo "Seed: $SEED"
+
 Check() {
+    START_PREC=$2
     MAX_PREC=$1
-    for MODE in "MCA" ; do #FIXME test with "RR"
-      for PREC in $(seq 3 10 $MAX_PREC) ; do
+    for MODE in "PB" "RR" "MCA" ; do
+	for PREC in $(seq $START_PREC 10 $MAX_PREC) ; do
+	    echo "Checking at PRECISION $PREC MODE $MODE"
+	    rm -f out_mpfr out_quad
+	    export VFC_BACKENDS="libinterflop_mca_mpfr.so --precision $PREC --mode $MODE --seed=$SEED"
+	    ./test > out_mpfr
 
-        echo "Checking at PRECISION $PREC MODE $MODE"
-        rm -f out_mpfr out_quad
-        export VFC_BACKENDS="libinterflop_mca_mpfr.so --precision $PREC --mode $MODE --seed=0"
-        ./test > out_mpfr
+	    export VFC_BACKENDS="libinterflop_mca.so --precision $PREC --mode $MODE --seed=$SEED"
+	    ./test > out_quad
 
-        export VFC_BACKENDS="libinterflop_mca.so --precision $PREC --mode $MODE --seed=0"
-        ./test > out_quad
-
-	diff out_mpfr out_quad
-	if [ $? -ne 0 ] ; then
-          echo "error"
-          exit 1
-        else
-          echo "ok for precision $PREC"
-        fi
-      done
+	    diff out_mpfr out_quad
+	    if [ $? -ne 0 ] ; then
+		echo "error"
+		exit
+	    else
+		echo "ok for precision $PREC"
+	    fi
+	done
     done
 }
 
@@ -31,9 +34,9 @@ Check() {
 for op in "+" "-" "*" "/" ; do
     echo "Checking $op float"
     verificarlo -D REAL=float -D SAMPLES=100 -D OPERATION="$op" -O0 test.c -o test
-    Check 24
+    Check 24 4
 
     echo "Checking $op double"
     verificarlo -D REAL=double -D SAMPLES=100 -D OPERATION="$op" -O0 test.c -o test
-    Check 53
+    Check 53 3
 done
