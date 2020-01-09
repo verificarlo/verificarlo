b7cb813d40aad55648afccb603d45433700e75fd
Marius Ghertescu <m.ghertescu@gmail.com>
2020-01-09 15:38:15 +0100

    Functional cancellation tests

--- tests/test_cancellation/compare.c
+++ tests/test_cancellation/compare.c
@@ -25,8 +25,11 @@ int is_wrong(const char* s1, const char* s2)
 
 int main(int argc, char const *argv[])
 {
-	FILE *f1 = fopen("RES/res_canc_detect_FLOAT", "r"), *f2 = fopen("OUTPUT/output_canc_detect_FLOAT", "r"), *out = fopen("test.log", "wr");
+	FILE *f1 =  fopen("RES/res_canc_detect_FLOAT", "r"),
+			 *f2 =  fopen("OUTPUT/output_canc_detect_FLOAT", "r"),
+			 *out = fopen("test.log", "wr");
 	int cpt = 0;
+	int f_scan_test;
 	assert(f1 != NULL);
 	assert(f2 != NULL);
 	assert(out != NULL);
@@ -35,33 +38,14 @@ int main(int argc, char const *argv[])
 	for(int i = 0; i < 22; i++){
 		for(int j = 0; j < 23; j++){
 			char s1[100] = {}, s2[100] = {};
-			fscanf(f1,"%s ",&s1);
-			fscanf(f2,"%s ",&s2);
+			f_scan_test = fscanf(f1,"%s ",(char*)&s1);
+			assert(f_scan_test > 0);
+			f_scan_test = fscanf(f2,"%s ",(char*)&s2);
+			assert(f_scan_test > 0);
 			if(is_wrong(s1,s2)){
-				fprintf(out,"line %d column %d \t- %s %s\n", i+1, j+1, s1, s2);	
-				cpt++;		
+				fprintf(out,"line %d column %d \t- %s %s\n", i+1, j+1, s1, s2);
+				cpt++;
 			}
-		}
-	}
-
-	fclose(f1);
-	fclose(f2);
-
-	f1 = fopen("RES/res_canc_exp_FLOAT", "r");
-	f2 = fopen("OUTPUT/output_canc_exp_FLOAT", "r");
-	assert(f1 != NULL);
-	assert(f2 != NULL);
-
-	fprintf(out,"OUTPUT/output_canc_exp_FLOAT\n");
-	for(int i = 0; i < 22; i++){
-		for(int j = 0; j < 23; j++){
-			char s1[100] = {}, s2[100] = {};
-			fscanf(f1,"%s ",&s1);
-			fscanf(f2,"%s ",&s2);
-			if(is_wrong(s1,s2)){
-				fprintf(out,"line %d column %d \t- %s %s\n", i+1, j+1, s1, s2);	
-				cpt++;		
-			}		
 		}
 	}
 
@@ -77,39 +61,19 @@ int main(int argc, char const *argv[])
 	for(int i = 0; i < 51; i++){
 		for(int j = 0; j < 52; j++){
 			char s1[100] = {}, s2[100] = {};
-			fscanf(f1,"%s ",&s1);
-			fscanf(f2,"%s ",&s2);
+			f_scan_test = fscanf(f1,"%s ",(char*)&s1);
+			assert(f_scan_test > 0);
+			f_scan_test = fscanf(f2,"%s ",(char*)&s2);
+			assert(f_scan_test > 0);
 			if(is_wrong(s1,s2)){
-				fprintf(out,"line %d column %d \t- %s %s\n", i+1, j+1, s1, s2);	
-				cpt++;		
-			}			
+				fprintf(out,"line %d column %d \t- %s %s\n", i+1, j+1, s1, s2);
+				cpt++;
+			}
 		}
 	}
 
 	fclose(f1);
 	fclose(f2);
-
-	f1 = fopen("RES/res_canc_exp_DOUBLE", "r");
-	f2 = fopen("OUTPUT/output_canc_exp_DOUBLE", "r");
-	assert(f1 != NULL);
-	assert(f2 != NULL);
-
-	fprintf(out,"OUTPUT/output_canc_exp_DOUBLE\n");
-	for(int i = 0; i < 51; i++){
-		for(int j = 0; j < 52; j++){
-			char s1[100] = {}, s2[100] = {};
-			fscanf(f1,"%s ",&s1);
-			fscanf(f2,"%s ",&s2);
-			if(is_wrong(s1,s2)){
-				fprintf(out,"line %d column %d \t- %s %s\n", i+1, j+1, s1, s2);	
-				cpt++;		
-			}		
-		}
-	}
-
-	fclose(f1);
-	fclose(f2);
-	fclose(out);
 
 	return (cpt/((22.0*23.0)*2.0 + (51.0*52.0)*2.0) >= 0.2);
 }
--- tests/test_cancellation/test.sh
+++ tests/test_cancellation/test.sh
@@ -1,21 +1,17 @@
 verificarlo -D FLOAT test_canc_detect.c -o test_canc_detect -Wall -O3 -ftree-vectorize
-verificarlo -D FLOAT test_canc_exp.c -o test_canc_exp -Wall -O3 -ftree-vectorize
 rm -f OUTPUT/output_canc_detect_FLOAT
 rm -f OUTPUT/output_canc_exp_FLOAT
-for i in {0..22}; do
+for i in `seq 0 22`; do
 	export VFC_BACKENDS="libinterflop_cancellation.so --precision $i"
     ./test_canc_detect >> OUTPUT/output_canc_detect_FLOAT
-    ./test_canc_exp >> OUTPUT/output_canc_exp_FLOAT
 done
 
 verificarlo -D DOUBLE test_canc_detect.c -o test_canc_detect -Wall -O3 -ftree-vectorize
-verificarlo -D DOUBLE test_canc_exp.c -o test_canc_exp -Wall -O3 -ftree-vectorize
 rm -f OUTPUT/output_canc_detect_DOUBLE
 rm -f OUTPUT/output_canc_exp_DOUBLE
-for i in {0..51}; do
+for i in `seq 0 51`; do
 	export VFC_BACKENDS="libinterflop_cancellation.so --precision $i"
     ./test_canc_detect >> OUTPUT/output_canc_detect_DOUBLE
-    ./test_canc_exp >> OUTPUT/output_canc_exp_DOUBLE
 done
 
 make compare
