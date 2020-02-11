77bd5bf387371a9c143136f55b44637c0bfd15e7
yohanchatelain <yohan.chatelain@gmail.com>
2020-02-11 15:35:29 +0100

    Applies changes requested by reviewer

--- src/backends/interflop-ieee/printf_specifier.c
+++ src/backends/interflop-ieee/printf_specifier.c
@@ -1,3 +1,27 @@
+/*****************************************************************************
+ *                                                                           *
+ *  This file is part of Verificarlo.                                        *
+ *                                                                           *
+ *  Copyright (c) 2015-2020                                                  *
+ *     Verificarlo contributors                                              *
+ *     Universite de Versailles St-Quentin-en-Yvelines                       *
+ *     CMLA, Ecole Normale Superieure de Cachan                              *
+ *                                                                           *
+ *  Verificarlo is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by     *
+ *  the Free Software Foundation, either version 3 of the License, or        *
+ *  (at your option) any later version.                                      *
+ *                                                                           *
+ *  Verificarlo is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU General Public License for more details.                             *
+ *                                                                           *
+ *  You should have received a copy of the GNU General Public License        *
+ *  along with Verificarlo.  If not, see <http://www.gnu.org/licenses/>.     *
+ *                                                                           *
+ *****************************************************************************/
+
 #include <endian.h>
 #include <math.h>
 #include <printf.h>
