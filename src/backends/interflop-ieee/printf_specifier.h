ee317ab9f939af897fe63120ad31b3cd63c5ebe1
yohanchatelain <yohan.chatelain@gmail.com>
2020-02-11 14:30:17 +0100

    Update printf_specifier.h
    Add comment for the `register_printf_bit` function

--- src/backends/interflop-ieee/printf_specifier.h
+++ src/backends/interflop-ieee/printf_specifier.h
@@ -1,6 +1,8 @@
 #ifndef __PRINTF_SPECIFIER_H__
 #define __PRINTF_SPECIFIER_H__
 
+/* This function defines the conversion specifier character 'b' */
+/* Once registered, %b can be used as %f for floating point values in printf functions */
 void register_printf_bit(void);
 
 #endif /* __PRINTF_SPECIFIER_H__ */
