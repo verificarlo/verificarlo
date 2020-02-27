4c84179b7e847184647c03eb10b260b90dcf50ac
Alan Vaquet <alan.vaquet@gmail.com>
2020-02-27 11:42:34 +0100

    fix test for the cancellation backend

--- tests/test_cancellation/test.sh
+++ tests/test_cancellation/test.sh
@@ -1,18 +1,29 @@
+SEED=5929
+#1234
+
 verificarlo -D FLOAT test_canc_detect.c -o test_canc_detect -Wall 
 rm -f OUTPUT/output_canc_detect_FLOAT
-rm -f OUTPUT/output_canc_exp_FLOAT
 for i in `seq 0 22`; do
-	export VFC_BACKENDS="libinterflop_cancellation.so --tolerance $i"
+	export VFC_BACKENDS="libinterflop_cancellation.so --tolerance $i --seed=$SEED"
     ./test_canc_detect >> OUTPUT/output_canc_detect_FLOAT
 done
 
 verificarlo -D DOUBLE test_canc_detect.c -o test_canc_detect -Wall 
 rm -f OUTPUT/output_canc_detect_DOUBLE
-rm -f OUTPUT/output_canc_exp_DOUBLE
 for i in `seq 0 51`; do
-	export VFC_BACKENDS="libinterflop_cancellation.so --tolerance $i"
+	export VFC_BACKENDS="libinterflop_cancellation.so --tolerance $i --seed=$SEED"
     ./test_canc_detect >> OUTPUT/output_canc_detect_DOUBLE
 done
 
-make compare
-./compare
+rm -f test.log
+diff  OUTPUT/output_canc_detect_DOUBLE RES/res_canc_detect_DOUBLE >> test.log
+diff  OUTPUT/output_canc_detect_FLOAT RES/res_canc_detect_FLOAT >> test.log
+
+res=`wc -l < test.log`
+
+if [ $res != 0 ] ; then
+echo "error"
+exit 1
+else
+echo "ok"
+fi
