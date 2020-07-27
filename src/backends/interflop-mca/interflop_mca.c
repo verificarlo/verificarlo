a41ac547bc23da18bc36f3e6b09142ade740e3fe
Matei Istoan <matei.istoan@inria.fr>
2020-07-27 22:52:13 +0200

    work in progress on adding the absolute error target as an option

--- src/backends/interflop-mca/interflop_mca.c
+++ src/backends/interflop-mca/interflop_mca.c
@@ -78,6 +78,7 @@ typedef enum {
   KEY_ERR_EXP,
   KEY_MODE = 'm',
   KEY_ERR_MODE = 'e',
+  KEY_ERR_EXP,
   KEY_SEED = 's',
   KEY_DAZ = 'd',
   KEY_FTZ = 'f'
@@ -231,14 +232,13 @@ static __float128 _noise_binary128(const int exp) {
       return;                                                                  \
     } else {                                                                   \
       if (((t_context *)CTX)->relErr) {                                        \
-        const int32_t e_a = GET_EXP_FLT(*X);                                   \
-        const int32_t e_n_rel = e_a - (VIRTUAL_PRECISION - 1);                 \
-        const typeof(*X) noise_rel = _NOISE(*X, e_n_rel);                      \
+        e_n_rel = e_a - (VIRTUAL_PRECISION - 1);                               \
+        noise_rel = _NOISE(*X, e_n_rel);                                       \
         *X = *X + noise_rel;                                                   \
       }                                                                        \
       if (((t_context *)CTX)->absErr) {                                        \
-        const int32_t e_n_abs = ((t_context *)CTX)->absErr_exp;                \
-        const typeof(*X) noise_abs = _NOISE(*X, e_n_abs);                      \
+        e_n_abs = ((t_context *)CTX)->absErr_exp;                              \
+        noise_abs = _NOISE(*X, e_n_abs);                                       \
         *X = *X + noise_abs;                                                   \
       }                                                                        \
     }                                                                          \
