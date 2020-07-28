e8dd34b49c68de5720964e6fa2f82cbe2053ccee
Matei Istoan <matei.istoan@inria.fr>
2020-07-28 23:33:14 +0200

    fix for code formatting

--- src/backends/interflop-mca/interflop_mca.c
+++ src/backends/interflop-mca/interflop_mca.c
@@ -499,26 +499,27 @@ void init_context(t_context *ctx) {
 void print_information_header(void *context) {
   t_context *ctx = (t_context *)context;
 
-  logger_info("load backend with "
-              "%s = %d, "
-              "%s = %d, "
-              "%s = %s, "
-              "%s = %s, "
-              "%s = %d, "
-              "%s = %s and "
-              "%s = %s"
-              "\n",
-              key_prec_b32_str, MCALIB_BINARY32_T, key_prec_b64_str, MCALIB_BINARY64_T, 
-              key_mode_str, MCA_MODE_STR[MCALIB_MODE], key_err_mode_str, 
-              (ctx->relErr && !ctx->absErr)
-                  ? MCA_ERR_MODE_STR[mca_err_mode_rel]
-                  : (!ctx->relErr && ctx->absErr)
-                        ? MCA_ERR_MODE_STR[mca_err_mode_abs]
-                        : (ctx->relErr && ctx->absErr)
-                              ? MCA_ERR_MODE_STR[mca_err_mode_all]
-                              : MCA_ERR_MODE_STR[mca_err_mode_rel],
-              key_err_exp_str, (ctx->absErr_exp), key_daz_str, 
-              ctx->daz ? "true" : "false", key_ftz_str, ctx->ftz ? "true" : "false");
+  logger_info(
+      "load backend with "
+      "%s = %d, "
+      "%s = %d, "
+      "%s = %s, "
+      "%s = %s, "
+      "%s = %d, "
+      "%s = %s and "
+      "%s = %s"
+      "\n",
+      key_prec_b32_str, MCALIB_BINARY32_T, key_prec_b64_str, MCALIB_BINARY64_T, 
+      key_mode_str, MCA_MODE_STR[MCALIB_MODE], key_err_mode_str, 
+      (ctx->relErr && !ctx->absErr)
+          ? MCA_ERR_MODE_STR[mca_err_mode_rel]
+          : (!ctx->relErr && ctx->absErr)
+                ? MCA_ERR_MODE_STR[mca_err_mode_abs]
+                : (ctx->relErr && ctx->absErr)
+                      ? MCA_ERR_MODE_STR[mca_err_mode_all]
+                      : MCA_ERR_MODE_STR[mca_err_mode_rel],
+      key_err_exp_str, (ctx->absErr_exp), key_daz_str, 
+      ctx->daz ? "true" : "false", key_ftz_str, ctx->ftz ? "true" : "false");
 }
 
 struct interflop_backend_interface_t interflop_init(int argc, char **argv,
