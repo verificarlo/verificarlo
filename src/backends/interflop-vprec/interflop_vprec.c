96e0a8ce840fe66768d87f0fbe7c08f0ab9c439b
Matei Istoan <matei.istoan@inria.fr>
2020-08-05 01:13:33 +0200

    bugfix: formatting fix

--- src/backends/interflop-vprec/interflop_vprec.c
+++ src/backends/interflop-vprec/interflop_vprec.c
@@ -421,16 +421,18 @@ static float _vprec_round_binary32(float a, char is_input, void *context,
       (((t_context *)context)->ftz && !is_input)) {
     a = 0;
   } else {
-    if ((((t_context *)context)->relErr == true) 
-        && (((t_context *)context)->absErr == true)) {
+    if ((((t_context *)context)->relErr == true) &&
+        (((t_context *)context)->absErr == true)) {
       /* vprec error mode all */
-      if ((-1)*((t_context *)context)->absErr_exp < binary32_precision)
-        a = handle_binary32_denormal(a, emin, aexp.u32, (-1)*((t_context *)context)->absErr_exp);
+      if ((-1) * ((t_context *)context)->absErr_exp < binary32_precision)
+        a = handle_binary32_denormal(a, emin, aexp.u32,
+                                     (-1) * ((t_context *)context)->absErr_exp);
       else
         a = handle_binary32_denormal(a, emin, aexp.u32, binary32_precision);
     } else if (((t_context *)context)->absErr == true) {
       /* vprec error mode abs */
-      a = handle_binary32_denormal(a, emin, aexp.u32, (-1)*((t_context *)context)->absErr_exp);
+      a = handle_binary32_denormal(a, emin, aexp.u32,
+                                   (-1) * ((t_context *)context)->absErr_exp);
     }
   }
 
@@ -513,6 +515,29 @@ static double _vprec_round_binary64(double a, char is_input, void *context,
   /* check for overflow in target range */
   if (aexp.s64 > emax) {
     a = a * INFINITY;
+    sp_case = true;
+  }
+
+  if (aexp.s64 <= emin) {
+    if ((((t_context *)context)->relErr == true) &&
+        (((t_context *)context)->absErr == true)) {
+      /* vprec error mode all */
+      if ((-1) * ((t_context *)context)->absErr_exp < binary64_precision)
+        a = handle_binary64_denormal(a, emin, aexp.u64,
+                                     (-1) * ((t_context *)context)->absErr_exp);
+      else
+        a = handle_binary64_denormal(a, emin, aexp.u64, binary64_precision);
+    } else if (((t_context *)context)->absErr == true) {
+      /* vprec error mode abs */
+      a = handle_binary64_denormal(a, emin, aexp.u64,
+                                   (-1) * ((t_context *)context)->absErr_exp);
+    }
+  }
+
+  /* Special ops must be placed after denormal handling  */
+  /* If the operand raises an underflow, the operation */
+  /* has a different behavior. Example: x*Inf != 0*Inf */
+  if (sp_case) {
     return a;
   }
 
@@ -1488,10 +1513,9 @@ void print_information_header(void *context) {
                 : (ctx->relErr && ctx->absErr)
                       ? VPREC_ERR_MODE_STR[vprec_err_mode_all]
                       : VPREC_ERR_MODE_STR[vprec_err_mode_rel],
-      key_err_exp_str, (ctx->absErr_exp), 
-      key_daz_str, ctx->daz ? "true" : "false",
-      key_ftz_str, ctx->ftz ? "true" : "false", key_instrument_str,
-      VPREC_INST_MODE_STR[VPREC_INST_MODE]);
+      key_err_exp_str, (ctx->absErr_exp), key_daz_str,
+      ctx->daz ? "true" : "false", key_ftz_str, ctx->ftz ? "true" : "false",
+      key_instrument_str, VPREC_INST_MODE_STR[VPREC_INST_MODE]);
 }
 
 void _interflop_finalize(void *context) {
