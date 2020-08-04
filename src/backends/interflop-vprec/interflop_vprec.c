38e08412e2e89bf697ed1c31cb7cf082fc452436
Matei Istoan <matei.istoan@inria.fr>
2020-08-05 00:41:10 +0200

    refining the formula; binary32, for now

--- src/backends/interflop-vprec/interflop_vprec.c
+++ src/backends/interflop-vprec/interflop_vprec.c
@@ -100,10 +100,10 @@ static const char *VPREC_MODE_STR[] = {"ieee", "full", "ib", "ob"};
 
 /* define the available error modes */
 typedef enum {
-  mca_err_mode_rel,
-  mca_err_mode_abs,
-  mca_err_mode_all,
-  _mca_err_mode_end_
+  vprec_err_mode_rel,
+  vprec_err_mode_abs,
+  vprec_err_mode_all,
+  _vprec_err_mode_end_
 } vprec_err_mode;
 
 static const char *VPREC_ERR_MODE_STR[] = {"rel", "abs", "all"};
@@ -300,10 +300,15 @@ static float _vprec_round_binary32(float a, char is_input, void *context,
   // here emin is the smallest exponent in the *normal* range
   int emin = 1 - emax;
 
+
+
   /* in absolute error mode, the error threshold also gives the possible underflow limit */
-  if ((t_context *)context)->absErr == true) {
-    if ((t_context *)context)->absErr_exp > emin)
-      emin = t_context->absErr_exp;
+  if ((((t_context *)context)->relErr == true) 
+      && (((t_context *)context)->absErr == true)) {
+    if (((t_context *)context)->absErr_exp > emin)
+      emin = ((t_context *)context)->absErr_exp;
+  } else if (((t_context *)context)->absErr == true) {
+    emin = ((t_context *)context)->absErr_exp;
   }
 
   binary32 aexp = {.f32 = a};
@@ -345,9 +350,9 @@ static double _vprec_round_binary64(double a, char is_input, void *context,
   int emin = 1 - emax;
 
   /* in absolute error mode, the error threshold also gives the possible underflow limit */
-  if ((t_context *)context)->absErr == true) {
-    if ((t_context *)context)->absErr_exp > emin)
-      emin = t_context->absErr_exp;
+  if (((t_context *)context)->absErr == true) {
+    if (((t_context *)context)->absErr_exp > emin)
+      emin = ((t_context *)context)->absErr_exp;
   }
 
   binary64 aexp = {.f64 = a};
