9683f624d88009bd5d876ce5afc608001de775be
Matei Istoan <matei.istoan@inria.fr>
2020-08-04 23:54:48 +0200

    work in progress on adding support for absolute error mode for vprec

--- src/backends/interflop-vprec/interflop_vprec.c
+++ src/backends/interflop-vprec/interflop_vprec.c
@@ -103,16 +103,9 @@ static const char *VPREC_MODE_STR[] = {"ieee", "full", "ib", "ob"};
 
 /* define the available error modes */
 typedef enum {
-<<<<<<< HEAD
   vprec_err_mode_rel,
   vprec_err_mode_abs,
   vprec_err_mode_all
-=======
-  mca_err_mode_rel,
-  mca_err_mode_abs,
-  mca_err_mode_all,
-  _mca_err_mode_end_
->>>>>>> work in progress on adding support for absolute error mode for vprec
 } vprec_err_mode;
 
 static const char *VPREC_ERR_MODE_STR[] = {"rel", "abs", "all"};
@@ -1374,6 +1367,10 @@ static struct argp_option options[] = {
     {key_log_file_str, KEY_LOG_FILE, "LOG", 0,
      "log file where input/output informations are written", 0},
     {key_mode_str, KEY_MODE, "MODE", 0,
+    {key_err_mode_str, KEY_ERR_MODE, "ERROR_MODE", 0,
+     "select error mode among {rel, abs, all}", 0},
+    {key_err_exp_str, KEY_ERR_EXP, "MAX_ABS_ERROR_EXPONENT", 0,
+     "select magnitude of the maximum absolute error", 0},
      "select VPREC mode among {ieee, full, ib, ob}", 0},
     {key_err_mode_str, KEY_ERR_MODE, "ERROR_MODE", 0,
      "select error mode among {rel, abs, all}", 0},
@@ -1588,9 +1585,16 @@ void print_information_header(void *context) {
                 : (ctx->relErr && ctx->absErr)
                       ? VPREC_ERR_MODE_STR[vprec_err_mode_all]
                       : VPREC_ERR_MODE_STR[vprec_err_mode_rel],
+<<<<<<< HEAD
       key_err_exp_str, (ctx->absErr_exp), key_daz_str,
       ctx->daz ? "true" : "false", key_ftz_str, ctx->ftz ? "true" : "false",
       key_instrument_str, VPREC_INST_MODE_STR[VPREC_INST_MODE]);
+=======
+      key_err_exp_str, (ctx->absErr_exp), 
+      key_daz_str, ctx->daz ? "true" : "false",
+      key_ftz_str, ctx->ftz ? "true" : "false", key_instrument_str,
+      VPREC_INST_MODE_STR[VPREC_INST_MODE]);
+>>>>>>> work in progress on adding support for absolute error mode for vprec
 }
 
 void _interflop_finalize(void *context) {
