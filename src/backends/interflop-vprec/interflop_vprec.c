6bb475503991b1ed2d41ca664334ac1cdfd677ae
Matei Istoan <matei.istoan@inria.fr>
2020-08-05 00:46:56 +0200

    refining the formula; now also binary64

--- src/backends/interflop-vprec/interflop_vprec.c
+++ src/backends/interflop-vprec/interflop_vprec.c
@@ -446,8 +446,6 @@ static float _vprec_round_binary32(float a, char is_input, void *context,
   /* here emin is the smallest exponent in the *normal* range */
   int emin = 1 - emax;
 
-
-
   /* in absolute error mode, the error threshold also gives the possible underflow limit */
   if ((((t_context *)context)->relErr == true) 
       && (((t_context *)context)->absErr == true)) {
@@ -552,9 +550,12 @@ static double _vprec_round_binary64(double a, char is_input, void *context,
   int emin = 1 - emax;
 
   /* in absolute error mode, the error threshold also gives the possible underflow limit */
-  if (((t_context *)context)->absErr == true) {
+  if ((((t_context *)context)->relErr == true) 
+      && (((t_context *)context)->absErr == true)) {
     if (((t_context *)context)->absErr_exp > emin)
       emin = ((t_context *)context)->absErr_exp;
+  } else if (((t_context *)context)->absErr == true) {
+    emin = ((t_context *)context)->absErr_exp;
   }
 
   binary64 aexp = {.f64 = a};
