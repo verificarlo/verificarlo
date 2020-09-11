9ffc2644d3a7a62db774271f0d490a02664ce8a2
Pablo Oliveira <pablo@sifflez.org>
2020-09-11 10:26:47 +0200

    Fix python shebang and np interface

--- tests/test_vprec_backend/generate_input.py
+++ tests/test_vprec_backend/generate_input.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import numpy as np
 import sys
@@ -9,24 +9,24 @@ def print_random_fp(n, r, output_filename):
 
     #the function will generate a minimum of 3 numbers
     n_s=int(max(n/3,1))
-    reminder=max(0,n-3*n_s) 
+    reminder=max(0,n-3*n_s)
     #cannot exceed float exponent because of numpy random uniform
     #furthermore we are generating float which means when testing double that the mantissa will have zero's after bit 23, which is not great to test rounding...
     emax = 2**(r-1)-1.0
     #emax = min(2**(r-1)-1,127)
     #any number in the representable range
-    rand_fp_array_1 = np.random.default_rng().uniform(low=-2**(emax-1),high=2**(emax-1), size=n_s+reminder)
-    rand_fp_array_2 = np.random.default_rng().uniform(low=-2**(emax-1),high=2**(emax-1), size=n_s+reminder)
+    rand_fp_array_1 = np.random.uniform(low=-2**(emax-1),high=2**(emax-1), size=n_s+reminder)
+    rand_fp_array_2 = np.random.uniform(low=-2**(emax-1),high=2**(emax-1), size=n_s+reminder)
 
     #add small numbers with negative exponent
-    rand_small_fp_array_1 = np.random.default_rng().uniform(low=-1,high=1, size=n_s)
-    rand_small_fp_array_2 = np.random.default_rng().uniform(low=-1,high=1, size=n_s)
-    
+    rand_small_fp_array_1 = np.random.uniform(low=-1,high=1, size=n_s)
+    rand_small_fp_array_2 = np.random.uniform(low=-1,high=1, size=n_s)
+
     #add denormals
     emin=1.0-emax;
-    rand_sub_fp_array_1 = np.random.default_rng().uniform(low=-2**(emin),high=2**(emin), size=n_s)
-    rand_sub_fp_array_2 = np.random.default_rng().uniform(low=-2**(emin),high=2**(emin), size=n_s)
-    
+    rand_sub_fp_array_1 = np.random.uniform(low=-2**(emin),high=2**(emin), size=n_s)
+    rand_sub_fp_array_2 = np.random.uniform(low=-2**(emin),high=2**(emin), size=n_s)
+
     fo = open(output_filename, "w")
 
     for fp1,fp2 in zip(rand_fp_array_1,rand_fp_array_2):
