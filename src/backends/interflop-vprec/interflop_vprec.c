7a8d934b1e7e79583b92364021ad281c879f2d9c
Matei Istoan <matei.istoan@inria.fr>
2020-09-22 15:09:48 +0200

    coherency in interpretting absErr_exp

--- src/backends/interflop-vprec/interflop_vprec.c
+++ src/backends/interflop-vprec/interflop_vprec.c
@@ -473,7 +473,7 @@ static float _vprec_round_binary32(float a, char is_input, void *context,
       if ((currentContext->relErr == true) &&
           (currentContext->absErr == true)) {
         /* vprec error mode all */
-        if ((-1) * currentContext->absErr_exp < binary32_precision)
+        if (abs(currentContext->absErr_exp) < binary32_precision)
           a = handle_binary32_denormal(a, emin, abs(currentContext->absErr_exp));
         else
           a = handle_binary32_denormal(a, emin, binary32_precision);
@@ -606,7 +606,7 @@ static double _vprec_round_binary64(double a, char is_input, void *context,
       if ((currentContext->relErr == true) &&
           (currentContext->absErr == true)) {
         /* vprec error mode all */
-        if ((-1) * currentContext->absErr_exp < binary64_precision)
+        if (abs(currentContext->absErr_exp) < binary64_precision)
           a = handle_binary64_denormal(a, emin, abs(currentContext->absErr_exp));
         else
           a = handle_binary64_denormal(a, emin, binary64_precision);
