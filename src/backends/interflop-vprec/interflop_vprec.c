29b97201b17ffe708e1f4e15595901318839246b
Matei Istoan <matei.istoan@inria.fr>
2020-11-19 20:48:15 +0100

    work in progress on factoring the code for computing the precision at which to round the floating-point numbers, using absolute error and relative error modes; work on  binary64

--- src/backends/interflop-vprec/interflop_vprec.c
+++ src/backends/interflop-vprec/interflop_vprec.c
@@ -505,11 +505,15 @@ static double _vprec_round_binary64(double a, char is_input, void *context,
 
   /* in absolute error mode, the error threshold also gives the possible
    * underflow limit */
-  if ((currentContext->relErr == true) && (currentContext->absErr == true)) {
-    if (currentContext->absErr_exp > emin)
+  if (currentContext->absErr == true) {
+    if (currentContext->relErr == true) {
+      /* relative and absolute error mode */
+      if (currentContext->absErr_exp > emin)
+        emin = currentContext->absErr_exp;
+    } else {
+      /* absolute error mode */
       emin = currentContext->absErr_exp;
-  } else if (currentContext->absErr == true) {
-    emin = currentContext->absErr_exp;
+    }
   }
 
   binary64 aexp = {.f64 = a};
@@ -519,29 +523,23 @@ static double _vprec_round_binary64(double a, char is_input, void *context,
   /* check for overflow in target range */
   if (aexp.s64 > emax) {
     a = a * INFINITY;
+    return a;
   }
 
   /* check for underflow in target range */
   if (aexp.s64 < emin) {
+    /* underflow case: possibly a denormal */
     if ((currentContext->daz && is_input) ||
         (currentContext->ftz && !is_input)) {
       a = 0;
     } else {
-      if ((currentContext->relErr == true) &&
-          (currentContext->absErr == true)) {
-        /* vprec error mode all */
-        if (abs(currentContext->absErr_exp) < binary64_precision)
-          a = handle_binary64_denormal(a, emin,
-                                       abs(currentContext->absErr_exp));
-        else
-          a = handle_binary64_denormal(a, emin, binary64_precision);
-      } else if (currentContext->absErr == true) {
-        /* vprec error mode abs */
-        a = handle_binary64_denormal(a, emin, abs(currentContext->absErr_exp));
-      } else {
-        /* vprec error mode rel */
-        a = handle_binary64_denormal(a, emin, binary64_precision);
-      }
+      int binary64_precision_adjusted;
+      if (currentContext->absErr == true)
+        binary64_precision_adjusted =
+            compute_absErr_vprec_binary64(true, context, 0, binary64_precision);
+      else
+        binary64_precision_adjusted = binary64_precision;
+      a = handle_binary64_denormal(a, emin, binary64_precision_adjusted);
     }
   }
 
