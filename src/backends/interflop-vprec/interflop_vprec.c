11222cb095afd82eeb9e8b886759fb312d327e33
Matei Istoan <matei.istoan@inria.fr>
2020-11-20 16:21:45 +0100

    fix after latest rebase

--- src/backends/interflop-vprec/interflop_vprec.c
+++ src/backends/interflop-vprec/interflop_vprec.c
@@ -1289,14 +1289,6 @@ static struct argp_option options[] = {
      "select error mode among {rel, abs, all}", 0},
     {key_err_exp_str, KEY_ERR_EXP, "MAX_ABS_ERROR_EXPONENT", 0,
      "select magnitude of the maximum absolute error", 0},
-<<<<<<< HEAD
-=======
-     "select VPREC mode among {ieee, full, ib, ob}", 0},
-    {key_err_mode_str, KEY_ERR_MODE, "ERROR_MODE", 0,
-     "select error mode among {rel, abs, all}", 0},
-    {key_err_exp_str, KEY_ERR_EXP, "MAX_ABS_ERROR_EXPONENT", 0,
-     "select magnitude of the maximum absolute error", 0},
->>>>>>> working on resolving the rebase; _vprec_round_binary64
     {key_instrument_str, KEY_INSTRUMENT, "INSTRUMENTATION", 0,
      "select VPREC instrumentation mode among {arguments, operations, full}",
      0},
