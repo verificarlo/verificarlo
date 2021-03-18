f4cb61b37b0c4f277229cafebc0773739d3a010b
Sholde <exshode@gmail.com>
2021-03-18 23:34:35 +0100

    adding new tests and check if clean.sh script is present

--- tests/clean.sh
+++ tests/clean.sh
@@ -5,11 +5,15 @@ tests="$tests $(find . -maxdepth 1 -type d -name 'test_*' | sort)"
 count=$(echo $tests | wc -w)
 
 # Print number of tests detected
-echo "1..$count"
+echo "1..$count tests"
 
-for t in $tests; do
-    echo $t
-    bash -c -l "cd $t && ./clean.sh"
+for t in $tests ; do
+    if [ -e $t/clean.sh ] ; then
+        echo $t OK
+        bash -c -l "cd $t && ./clean.sh"
+    else
+        echo $t have not clean.sh script
+    fi
 done
 
-rm -Rf *~ paths.sh testplan.log testplan.trs
+rm -Rf *~ paths.sh testplan.log testplan.trs test-suite.log
