0fcbc7e94e2c7120d0eeecbfd6ae78be3d9ad1b3
Sholde <exshode@gmail.com>
2021-06-01 15:59:50 +0200

    using float_type.h header in interflop.h

--- src/common/interflop.h
+++ src/common/interflop.h
@@ -2,27 +2,7 @@
 
 #include <stdint.h>
 
-/* Define vector type */
-typedef double double2 __attribute__((ext_vector_type(2)));
-typedef double double4 __attribute__((ext_vector_type(4)));
-typedef double double8 __attribute__((ext_vector_type(8)));
-typedef double double16 __attribute__((ext_vector_type(16)));
-typedef float float2 __attribute__((ext_vector_type(2)));
-typedef float float4 __attribute__((ext_vector_type(4)));
-typedef float float8 __attribute__((ext_vector_type(8)));
-typedef float float16 __attribute__((ext_vector_type(16)));
-typedef int int2 __attribute__((ext_vector_type(2)));
-typedef int int4 __attribute__((ext_vector_type(4)));
-typedef int int8 __attribute__((ext_vector_type(8)));
-typedef int int16 __attribute__((ext_vector_type(16)));
-typedef int32_t int32_2x __attribute__((ext_vector_type(2)));
-typedef int32_t int32_4x __attribute__((ext_vector_type(4)));
-typedef int32_t int32_8x __attribute__((ext_vector_type(8)));
-typedef int32_t int32_16x __attribute__((ext_vector_type(16)));
-typedef uint32_t uint32_2x __attribute__((ext_vector_type(2)));
-typedef uint32_t uint32_4x __attribute__((ext_vector_type(4)));
-typedef uint32_t uint32_8x __attribute__((ext_vector_type(8)));
-typedef uint32_t uint32_16x __attribute__((ext_vector_type(16)));
+#include "float_type.h"
 
 /* interflop float compare predicates, follows the same order than
  * LLVM's FCMPInstruction predicates */
