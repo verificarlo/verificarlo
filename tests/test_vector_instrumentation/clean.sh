ca2c981b83d817604e1a4a328167ca298247fa12
Sholde <exshode@gmail.com>
2021-06-08 16:26:48 +0200

    remove vector instrumentation tests

--- tests/test_vector_instrumentation/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-temporary="*~"
-binary="*.asm *.o bin"
-instrument="wrapper ieee vprec mca bitmask cancellation mca_mpfr"
-result="result.txt output*"
-
-rm -Rf $temporary $instrument $binary $result
