afa087bb52109464d0e55919edc78da1e3b38957
Sholde <exshode@gmail.com>
2021-06-14 21:46:38 +0200

    separate vector operations in cancellation

--- src/backends/interflop-cancellation/interflop_cancellation.c
+++ src/backends/interflop-cancellation/interflop_cancellation.c
@@ -106,25 +106,6 @@ static inline double _noise_binary64(const int exp) {
     }                                                                          \
   })
 
-#define define_interflop_add_sub_vector(size, precision, ops, ope)             \
-  static void _interflop_##ops##_##precision##_##size##x(                      \
-      precision##size *a, precision##size *b, precision##size *c,              \
-      void *context) {                                                         \
-    (*c) = (*a)ope(*b);                                                        \
-    for (int i = 0; i < size; ++i) {                                           \
-      precision _c = (*c)[i];                                                  \
-      cancell((*a)[i], (*b)[i], &_c);                                          \
-      (*c)[i] = _c;                                                            \
-    }                                                                          \
-  }
-
-#define define_interflop_mul_div_vector(size, precision, ops, ope)             \
-  static void _interflop_##ops##_##precision##_##size##x(                      \
-      precision##size *a, precision##size *b, precision##size *c,              \
-      void *context) {                                                         \
-    (*c) = (*a)ope(*b);                                                        \
-  }
-
 /* Cancellations can only happen during additions and substractions */
 static void _interflop_add_float(float a, float b, float *c, void *context) {
   *c = a + b;
@@ -148,32 +129,6 @@ static void _interflop_sub_double(double a, double b, double *c,
   cancell(a, b, c);
 }
 
-/* Define float add and sub vector functions */
-define_interflop_add_sub_vector(2, float, add, +);
-define_interflop_add_sub_vector(2, float, sub, -);
-
-define_interflop_add_sub_vector(4, float, add, +);
-define_interflop_add_sub_vector(4, float, sub, -);
-
-define_interflop_add_sub_vector(8, float, add, +);
-define_interflop_add_sub_vector(8, float, sub, -);
-
-define_interflop_add_sub_vector(16, float, add, +);
-define_interflop_add_sub_vector(16, float, sub, -);
-
-/* Define double add and sub vector functions */
-define_interflop_add_sub_vector(2, double, add, +);
-define_interflop_add_sub_vector(2, double, sub, -);
-
-define_interflop_add_sub_vector(4, double, add, +);
-define_interflop_add_sub_vector(4, double, sub, -);
-
-define_interflop_add_sub_vector(8, double, add, +);
-define_interflop_add_sub_vector(8, double, sub, -);
-
-define_interflop_add_sub_vector(16, double, add, +);
-define_interflop_add_sub_vector(16, double, sub, -);
-
 static void _interflop_mul_float(float a, float b, float *c, void *context) {
   *c = a * b;
 }
@@ -192,31 +147,8 @@ static void _interflop_div_double(double a, double b, double *c,
   *c = a / b;
 }
 
-/* Define float mul and div vector functions */
-define_interflop_mul_div_vector(2, float, mul, *);
-define_interflop_mul_div_vector(2, float, div, /);
-
-define_interflop_mul_div_vector(4, float, mul, *);
-define_interflop_mul_div_vector(4, float, div, /);
-
-define_interflop_mul_div_vector(8, float, mul, *);
-define_interflop_mul_div_vector(8, float, div, /);
-
-define_interflop_mul_div_vector(16, float, mul, *);
-define_interflop_mul_div_vector(16, float, div, /);
-
-/* Define double mul and div vector functions */
-define_interflop_mul_div_vector(2, double, mul, *);
-define_interflop_mul_div_vector(2, double, div, /);
-
-define_interflop_mul_div_vector(4, double, mul, *);
-define_interflop_mul_div_vector(4, double, div, /);
-
-define_interflop_mul_div_vector(8, double, mul, *);
-define_interflop_mul_div_vector(8, double, div, /);
-
-define_interflop_mul_div_vector(16, double, mul, *);
-define_interflop_mul_div_vector(16, double, div, /);
+// Include vector operations
+#include "interflop_cancellation_vector.h"
 
 static struct argp_option options[] = {
     {"tolerance", 't', "TOLERANCE", 0, "Select tolerance (TOLERANCE >= 0)", 0},
@@ -280,6 +212,8 @@ struct interflop_backend_interface_t interflop_init(int argc, char **argv,
   logger_info("interflop_cancellation: loaded backend with tolerance = %d\n",
               TOLERANCE);
 
+#ifdef __clang__
+
   struct interflop_backend_interface_t interflop_backend_cancellation = {
       _interflop_add_float,
       _interflop_sub_float,
@@ -334,6 +268,69 @@ struct interflop_backend_interface_t interflop_init(int argc, char **argv,
       NULL,
       NULL,
       NULL};
+
+#elif __GNUC__
+
+  struct interflop_backend_interface_t interflop_backend_cancellation = {
+      _interflop_add_float,
+      _interflop_sub_float,
+      _interflop_mul_float,
+      _interflop_div_float,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      _interflop_add_double,
+      _interflop_sub_double,
+      _interflop_mul_double,
+      _interflop_div_double,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL,
+      NULL};
+
+#else
+
+#error "Compiler must be gcc or clang"
+
+#endif
 
   /* Initialize the seed */
   _set_mca_seed(ctx->choose_seed, ctx->seed);
