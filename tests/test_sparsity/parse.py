f5df282b454995487b5952d0ecb841572a1b6425
gkiar <gkiar@precision>
2021-06-14 13:48:21 -0400

    added assertion that error is within a percent

--- tests/test_sparsity/parse.py
+++ tests/test_sparsity/parse.py
@@ -1,9 +1,22 @@
 #!/bin/env python3
 
+from argparse import ArgumentParser
 import numpy as np
 from collections import Counter
 
 if __name__ == '__main__':
-    x = np.loadtxt('log')
+    parser = ArgumentParser()
+    parser.add_argument("logfile")
+    parser.add_argument("sparsity", type=int)
+
+    args = parser.parse_args()
+
+    x = np.loadtxt(args.logfile)
     c = Counter(x)
-    print(c[8]*100.0/len(x))
+
+    percent_ieee = c[8]*100.0/len(x)
+    print(percent_ieee)
+    
+    rounded=round(percent_ieee, 0)
+    assert(rounded == 100*(1 - 1.0/args.sparsity)) 
+
--- tests/test_sparsity/test.sh
+++ tests/test_sparsity/test.sh
@@ -7,16 +7,23 @@ Check() {
       ./test 0x1.fffffffffffffp2  0x1.fffffffffffffp-50 >> log 2> /dev/null
   done
   DURATION=$[ ($(date +%s%N) - ${START})/1000000 ]
-  echo "Sparsity: ${sparsity} | $(./parse.py log)% IEEE (Time Elapsed: $DURATION ms)"
+  perc=`./parse.py log ${sparsity}`
+  if [ $? -eq 1 ]; then
+    ecode=1
+  fi
+  echo "Sparsity: ${sparsity} | ${perc}% IEEE (Time Elapsed: $DURATION ms)"
 }
 
 verificarlo-c -O0 test.c -o test
 export VFC_BACKENDS_SILENT_LOAD=True
 export VFC_BACKENDS_LOGGER=False
 export VFC_BACKENDS="libinterflop_mca.so --mode=mca --precision-binary64=24 --sparsity=100"
+ecode=0
 
 for sparsity in 1 2 4 10 100; do    
     rm -f log
     export VFC_BACKENDS="libinterflop_mca.so --mode=rr --precision-binary64=1 --sparsity=${sparsity}"
     Check
 done
+
+exit $ecode
