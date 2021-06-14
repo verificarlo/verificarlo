24864252fc421de9d72d79ff083d6ef56f4f1049
gkiar <gkiar@precision>
2021-06-14 17:10:54 -0400

    updated test to work for both floats and doubles

--- tests/test_sparsity/test.c
+++ tests/test_sparsity/test.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-void mainFloat(float a, float b) {
-  float sum = 0.0;
-  for (int i = 0; i < 100; i++) {
-    sum = a + b;
-    printf("%.13a\n", sum);
-  }
-}
-
-void mainDouble(double a, double b) {
-  double sum = 0.0;
-  for (int i = 0; i < 100; i++) {
-    sum = a + b;
-    printf("%.13a\n", sum);
-  }
-}
+#define FMT(X) _Generic((X), double: strtod, float: strtof)
 
 int main(int argc, char *argv[]) {
-  if (strcmp(argv[3], "float")) {
-    mainFloat(strtod(argv[1], NULL), strtod(argv[2], NULL));
-  } else {
-    mainDouble(strtod(argv[1], NULL), strtod(argv[2], NULL));
+  REAL sum = 0.0;
+  REAL a = FMT(sum)(argv[1], NULL);
+  REAL b = FMT(sum)(argv[2], NULL);
+  for (int i = 0; i < 100; i++) {
+    sum = a + b;
+    printf("%.13a\n", sum);
   }
 }
 
--- tests/test_sparsity/test.sh
+++ tests/test_sparsity/test.sh
@@ -4,7 +4,7 @@
 Check() {
   START="$(date +%s%N)"
   for i in `seq 1000` ; do
-      ./test 0x1.fffffffffffffp2  0x1.fffffffffffffp-50 ${dtype} >> log 2> /dev/null
+      ./test 0x1.fffffffffffffp2  0x1.fffffffffffffp-50 >> log 2> /dev/null
   done
   DURATION=$[ ($(date +%s%N) - ${START})/1000000 ]
   perc=`./parse.py log ${sparsity}`
@@ -14,13 +14,13 @@ Check() {
   echo "  Sparsity: ${sparsity} | ${perc}% IEEE (Time Elapsed: $DURATION ms)"
 }
 
-verificarlo-c -O0 test.c -o test
 export VFC_BACKENDS_SILENT_LOAD=True
 export VFC_BACKENDS_LOGGER=False
 ecode=0
 
 for dtype in "float" "double"; do
   echo $dtype | awk '{ print toupper($0) }'
+  verificarlo-c -D REAL=${dtype} -O0 test.c -o test
   for sparsity in 1 2 4 10 100; do    
     rm -f log
     export VFC_BACKENDS="libinterflop_mca.so --mode=rr --precision-binary64=1 --precision-binary32=1 --sparsity=${sparsity}"
