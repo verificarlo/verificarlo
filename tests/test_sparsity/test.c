c2471fbbe12d58e6143a6d13fa5e74cf291ddf1f
gkiar <gkiar@precision>
2021-06-14 16:00:03 -0400

    added test for floats, as well

--- tests/test_sparsity/test.c
+++ tests/test_sparsity/test.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-  double a = strtod(argv[1], NULL);
-  double b = strtod(argv[2], NULL);
+void mainFloat(float a, float b) {
+  float sum = 0.0;
+  for (int i = 0; i < 100; i++) {
+    sum = a + b;
+    printf("%.13a\n", sum);
+  }
+}
+
+void mainDouble(double a, double b) {
   double sum = 0.0;
   for (int i = 0; i < 100; i++) {
     sum = a + b;
     printf("%.13a\n", sum);
   }
 }
+
+int main(int argc, char *argv[]) {
+  if (strcmp(argv[3], "float")) {
+    mainFloat(strtod(argv[1], NULL), strtod(argv[2], NULL));
+  } else {
+    mainDouble(strtod(argv[1], NULL), strtod(argv[2], NULL));
+  }
+}
+
--- tests/test_sparsity/test.sh
+++ tests/test_sparsity/test.sh
@@ -4,26 +4,28 @@
 Check() {
   START="$(date +%s%N)"
   for i in `seq 1000` ; do
-      ./test 0x1.fffffffffffffp2  0x1.fffffffffffffp-50 >> log 2> /dev/null
+      ./test 0x1.fffffffffffffp2  0x1.fffffffffffffp-50 ${dtype} >> log 2> /dev/null
   done
   DURATION=$[ ($(date +%s%N) - ${START})/1000000 ]
   perc=`./parse.py log ${sparsity}`
   if [ $? -eq 1 ]; then
     ecode=1
   fi
-  echo "Sparsity: ${sparsity} | ${perc}% IEEE (Time Elapsed: $DURATION ms)"
+  echo "  Sparsity: ${sparsity} | ${perc}% IEEE (Time Elapsed: $DURATION ms)"
 }
 
 verificarlo-c -O0 test.c -o test
 export VFC_BACKENDS_SILENT_LOAD=True
 export VFC_BACKENDS_LOGGER=False
-export VFC_BACKENDS="libinterflop_mca.so --mode=mca --precision-binary64=24 --sparsity=100"
 ecode=0
 
-for sparsity in 1 2 4 10 100; do    
+for dtype in "float" "double"; do
+  echo $dtype | awk '{ print toupper($0) }'
+  for sparsity in 1 2 4 10 100; do    
     rm -f log
-    export VFC_BACKENDS="libinterflop_mca.so --mode=rr --precision-binary64=1 --sparsity=${sparsity}"
+    export VFC_BACKENDS="libinterflop_mca.so --mode=rr --precision-binary64=1 --precision-binary32=1 --sparsity=${sparsity}"
     Check
+  done
 done
 
 exit $ecode
