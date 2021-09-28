c9c8326501f0d23a59bad11e35d03f7c387eba0a
Matei Istoan <matei.istoan@inria.fr>
2021-09-28 15:00:27 +0200

    increased threshold for the sparsity test

--- tests/test_sparsity/parse.py
+++ tests/test_sparsity/parse.py
@@ -17,5 +17,5 @@ if __name__ == '__main__':
     percent_ieee = c[8]*100.0/len(x)
     print(percent_ieee)
 
-    assert(np.isclose(percent_ieee, 100*(1-args.sparsity), atol=1e-1))
+    assert(np.isclose(percent_ieee, 100*(1-args.sparsity), atol=2e-1))
 
