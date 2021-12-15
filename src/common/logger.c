5085c1ab06caf68e82c15c6a945a8457fef74afc
titeup <eric.petit@gmail.com>
2021-12-15 16:54:05 +0100

    fix copyright text and license in readme

--- src/common/logger.c
+++ src/common/logger.c
@@ -2,16 +2,21 @@
  *                                                                           *\
  *  This file is part of the Verificarlo project,                            *\
  *  under the Apache License v2.0 with LLVM Exceptions.                      *\
- *  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception                  *\
+ *  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception.                 *\
  *  See https://llvm.org/LICENSE.txt for license information.                *\
  *                                                                           *\
- *  Copyright (c) 2015-2018                                                  *\
+ *                                                                           *\
+ *  Copyright (c) 2015                                                       *\
  *     Universite de Versailles St-Quentin-en-Yvelines                       *\
  *     CMLA, Ecole Normale Superieure de Cachan                              *\
+ *                                                                           *\
+ *  Copyright (c) 2018                                                       *\
+ *     Universite de Versailles St-Quentin-en-Yvelines                       *\
+ *                                                                           *\
  *  Copyright (c) 2019-2021                                                  *\
  *     Verificarlo Contributors                                              *\
  *                                                                           *\
- *****************************************************************************/
+ ****************************************************************************/
 #include <err.h>
 #include <stdarg.h>
 #include <stdbool.h>
