af554cfe55396036fc37ce369eef785c0c056b53
Pablo Oliveira <pablo@sifflez.org>
2022-02-22 10:29:08 +0100

    Add parameter for iterations

--- tests/test_sr/check.py
+++ tests/test_sr/check.py
@@ -18,34 +18,42 @@ def read(name):
         freqs = freq(data)
     return freqs
 
+# TODO: Determine the tolerance precisely with a confidence interval depending
+# on the sampled iterations.
 TOLERANCE=15
 
 if __name__ == "__main__":
+    import sys
+    assert(len(sys.argv) == 2)
+    its = int(sys.argv[1])
     f = read("binary32-23")
     assert(len(f) == 1)
-    assert(f['0x1.4000020000000p+0'] == 100)
-    # For inexact24(1.0 + 2**-23) since the computed value is exact in 24 bits,
+    assert(f['0x1.4000020000000p+0'] == its)
+    # For inexact24(1.25 + 2**-23) since the computed value is exact in 24 bits,
     # SR (MCA RR 24) should preserve the value
 
     f = read("binary32-24")
     assert(len(f) == 2)
-    assert(50 - TOLERANCE < f['0x1.4000000000000p+0'] < 50 + TOLERANCE)
-    # For inexact24(1.0 + 2**-24) we are in the middle of the interval
+    target = its*50.0/100.
+    assert(target - TOLERANCE < f['0x1.4000000000000p+0'] < target + TOLERANCE)
+    # For inexact24(1.25 + 2**-24) we are in the middle of the interval
     # SR (MCA RR 24) should give us with 50% chance one of the two
     # upper and lower bounds 0x1.4000000000000p+0 and 0x1.4000020000000p+0
 
     f = read("binary32-25")
     assert(len(f) == 2)
-    assert(75 - TOLERANCE < f['0x1.4000000000000p+0'] < 75 + TOLERANCE)
-    # For inexact24(1.0 + 2**-25) we are in the quarter of the interval
+    target = its*75.0/100
+    assert(target - TOLERANCE < f['0x1.4000000000000p+0'] < target + TOLERANCE)
+    # For inexact24(1.25 + 2**-25) we are in the quarter of the interval
     # SR (MCA RR 24) should give us
     # upper and lower bounds 0x1.4000000000000p+0 and 0x1.4000020000000p+0
     # with probabilities 75% and 25% respectively
 
     f = read("binary32-26")
     assert(len(f) == 2)
-    assert(87.5 - TOLERANCE < f['0x1.4000000000000p+0'] < 87.5 + TOLERANCE)
-    # For inexact24(1.0 + 2**-26) we are in the eight of the interval
+    target = its*87.5/100
+    assert(target - TOLERANCE < f['0x1.4000000000000p+0'] < target + TOLERANCE)
+    # For inexact24(1.25 + 2**-26) we are in the eight of the interval
     # SR (MCA RR 24) should give us
     # upper and lower bounds 0x1.4000000000000p+0 and 0x1.4000020000000p+0
     # with probabilities 87.5% and 12.5% respectively
@@ -53,16 +61,19 @@ if __name__ == "__main__":
     # Assert similar properties for double precision
     f = read("binary64-52")
     assert(len(f) == 1)
-    assert(f['0x1.4000000000001p+0'] == 100)
+    assert(f['0x1.4000000000001p+0'] == its)
 
     f = read("binary64-53")
     assert(len(f) == 2)
-    assert(50 - TOLERANCE < f['0x1.4000000000000p+0'] < 50 + TOLERANCE)
+    target = its*50.0/100
+    assert(target - TOLERANCE < f['0x1.4000000000000p+0'] < target + TOLERANCE)
 
     f = read("binary64-54")
     assert(len(f) == 2)
-    assert(75 - TOLERANCE < f['0x1.4000000000000p+0'] < 75 + TOLERANCE)
+    target = its*75.0/100
+    assert(target - TOLERANCE < f['0x1.4000000000000p+0'] < target + TOLERANCE)
 
     f = read("binary64-55")
     assert(len(f) == 2)
-    assert(87.5 - TOLERANCE < f['0x1.4000000000000p+0'] < 87.5 + TOLERANCE)
+    target = its*87.5/100
+    assert(target - TOLERANCE < f['0x1.4000000000000p+0'] < target + TOLERANCE)
--- tests/test_sr/sr-binary32.c
+++ tests/test_sr/sr-binary32.c
@@ -9,7 +9,7 @@ int main(int argc, char * argv[]){
     float a = 1.25;
     float b = pow(2, -p);
 
-    for (int i=0; i < 100; i++) {
+    for (int i=0; i < ITERATIONS; i++) {
       printf("%.13a\n", a+b);
     }
     return 0;
