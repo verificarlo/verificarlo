7d4c0d8f22558748b63e551b7f0c37f6b8ef779f
yohanchatelain <yohan.chatelain@gmail.com>
2022-02-24 12:32:55 -0500

    Fix pointer issue and add subtests in test_usercall_inexact

--- tests/test_usercall_inexact/test.sh
+++ tests/test_usercall_inexact/test.sh
@@ -13,9 +13,10 @@ run_test() {
     BACKEND=$1
     REAL=$2
     N=$3
-    RES=$4
+    P=$4
+    RES=$5
     echo "Test ${BACKEND}"
-    verificarlo-c test.c -DREAL=${REAL} -DN=${N} -o test_${REAL}
+    verificarlo-c test.c -DREAL=${REAL} -DN=${N} -DP=${P} -o test_${REAL}
     VFC_BACKENDS="${BACKEND}" ./test_${REAL} 2>log.${REAL}
     grep "Before" log.${REAL} | grep -o '0x[0-9].[0-9a-f]*p[-+][0-9]*' >bfr.${REAL}
     grep "After" log.${REAL} | grep -o '0x[0-9].[0-9a-f]*p[-+][0-9]*' >aft.${REAL}
@@ -28,10 +29,22 @@ run_test() {
 
 clean
 for REAL in float double; do
-    run_test "libinterflop_ieee.so" $REAL $N 1
+    # IEEE backend, no noise must be introduced
+    run_test "libinterflop_ieee.so" $REAL $N 0 1
     clean
-    run_test "libinterflop_mca.so" $REAL $N 0
+    # MCA backend, inexact(virtual_precision=0) equivalent to use the current virtual precision
+    # By default 53 and 24 bits so it is equivalent to not perturb the value (since RR preserves FPs representable on p bits)
+    run_test "libinterflop_mca.so" $REAL $N 0 1
     clean
-    run_test "libinterflop_mca.so -m ieee" $REAL $N 1
+    # MCA backend, inexact(virtual_precision=-1) equivalent to use the current virtual precision - 1
+    # Equivalent to use 52 and 23 bits of virtual precision. Results must be slighty different
+    run_test "libinterflop_mca.so" $REAL $N -1 0
+    clean
+    # MCA backend, inexact(virtual_precision=1) equivalent to use a virtual precision of 1
+    # The minimal virtual precision must introduce a large noise.
+    run_test "libinterflop_mca.so" $REAL $N 1 0
+    clean
+    # MCA backend within IEEE mode must not introduce noise at all.
+    run_test "libinterflop_mca.so -m ieee" $REAL $N -1 1
     clean
 done
