1720728a5f51164229d2a205a8fe9a58876d942f
Yohan Chatelain <yohan.chatelain@gmail.com>
2023-03-18 20:03:42 -0400

    Use binomial test for test_sr

--- tests/test_sr/test.sh
+++ tests/test_sr/test.sh
@@ -12,29 +12,28 @@ verificarlo-c -DITERATIONS=$ITERATIONS -O0 sr-binary64.c -o sr-binary64 -lm
 
 for BACKEND in libinterflop_mca.so libinterflop_mca_int.so; do
 
-
-for p in 23 24 25 26; do
-  for PREC in "--mode=rr --precision-binary32=24"; do
-    VFC_BACKENDS="$BACKEND $PREC" ./sr-binary32 $p > binary32-$p
+  for p in 23 24 25 26; do
+    for PREC in "--mode=rr --precision-binary32=24"; do
+      VFC_BACKENDS="$BACKEND $PREC" ./sr-binary32 $p >binary32-$p
+    done
   done
-done
 
-for p in 52 53 54 55; do
-  for PREC in "--mode=rr --precision-binary64=53"; do
-    VFC_BACKENDS="$BACKEND $PREC" ./sr-binary64 $p > binary64-$p
+  for p in 52 53 54 55; do
+    for PREC in "--mode=rr --precision-binary64=53"; do
+      VFC_BACKENDS="$BACKEND $PREC" ./sr-binary64 $p >binary64-$p
+    done
   done
-done
 
-./check.py $ITERATIONS
+  ./check.py $ITERATIONS
 
-status=$?
+  status=$?
 
-if [ $status -eq 0 ]; then
-  echo "Success with backend $BACKEND!"
-else
-  echo "Failed with backend $BACKEND"
-  exit 1
-fi
+  if [ $status -eq 0 ]; then
+    echo "Success with backend $BACKEND!"
+  else
+    echo "Failed with backend $BACKEND"
+    exit 1
+  fi
 
 done
 
