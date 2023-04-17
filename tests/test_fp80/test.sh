84ccc00272119b58692682dce51b003acd7040f8
Pablo Oliveira <pablo@sifflez.org>
2023-04-17 17:46:43 +0200

    fix test_fp80 on aarch64

--- tests/test_fp80/test.sh
+++ tests/test_fp80/test.sh
@@ -4,11 +4,13 @@ set -e
 # Verificarlo should not fail
 verificarlo-c -O2 -c rotg.c 2> error
 
-# But is should report the unsupported type
-if grep "Unsupported operand type: x86_fp80" error; then
-  echo "verificarlo-c correctly reported unsupported x86_fp80 type"
-  exit 0
-else
-  echo "verificarlo-c failed silently"
-  exit 1
+if [[ $(arch) == "x86_64" ]]; then
+    # But is should report the unsupported FP80 type on x86 
+    if grep "Unsupported operand type: x86_fp80" error; then
+      echo "verificarlo-c correctly reported unsupported x86_fp80 type"
+      exit 0
+    else
+      echo "verificarlo-c failed silently"
+      exit 1
+    fi
 fi
