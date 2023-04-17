8f6dd0079a56f85b5e5bc0f5d391e87c374d28b5
Pablo Oliveira <pablo@sifflez.org>
2023-04-17 17:44:01 +0200

    Fix test_branch_instrumentation in aarch64

--- tests/test_branch_instrumentation/test.c
+++ tests/test_branch_instrumentation/test.c
@@ -7,9 +7,12 @@ bool f_ole(double a, double b) {
   return (a<=b);
 }
 
+// vectorized comparisons in aarch64 are not supported in LLVM15
+#if (__x86_64__)
 typedef double double4 __attribute__((ext_vector_type(4)));
 typedef long long4 __attribute__((ext_vector_type(4)));
 
 long4 f4x_ogt(double4 a, double4 b) {
   return (a > b);
 }
+#endif
