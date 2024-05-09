4abdc639324b4889816b1432b8e77936e88752fa
Yohan Chatelain <yohan.chatelain@gmail.com>
2024-05-09 13:03:41 -0400

    Make fma working with architectures that do not have libquadmath

--- src/interflop-stdlib/fma/interflop_fma.c
+++ src/interflop-stdlib/fma/interflop_fma.c
@@ -1,6 +1,7 @@
 #include <quadmath.h>
 
 #include "interflop_fma.h"
+#include "pfp128.h"
 
 float interflop_fma_binary32(float a, float b, float c) {
   return __builtin_fmaf(a, b, c);
@@ -11,7 +12,9 @@ double interflop_fma_binary64(double a, double b, double c) {
 }
 
 _Float128 interflop_fma_binary128(_Float128 a, _Float128 b, _Float128 c) {
+#ifdef HAS_QUADMATH
   return fmaq(a, b, c);
+#else
+  return fmaFP128(a, b, c);
+#endif
 }
-
-int main(int argc, char *argv) { return 0; }
